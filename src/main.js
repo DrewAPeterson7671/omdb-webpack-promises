b78e4859f13711a1924d6156ac1b987af0599fb4
Student <example@example.com>
2019-11-20 13:58:23 -0800

    Overcome api UI bug

--- src/main.js
+++ src/main.js
@@ -5,22 +5,30 @@ import './styles.css';
 
 $(document).ready(function() {
   $('#weatherLocation').click(function() {
-    const city = $('#location').val();
+    let city = $('#location').val();
     $('#location').val("");
 
-    fetch(`http://www.omdbapi.com/?t=${city}&apikey=${process.env.API_KEY}`)
-      .then(function(response) {
-        return response.json();
-      })
-      .then(function(jsonifiedResponse) {
-        getElements(jsonifiedResponse);
-      });
 
-   const getElements = function(response) {
-      $('.showHumidity').text(`${response.Title}`);
-       $('.showTemp').text(`${response.Director}`);
-      // $('.showHumidity').text(`The humidity in ${city} is ${response.title}%`);
-      // $('.showTemp').text(`The temperature in Kelvins is ${response.main.temp} degrees.`);
-    }
+    let promise = new Promise(function(resolve, reject) {
+      let request = new XMLHttpRequest();
+      let url = `http://www.omdbapi.com/?t=${city}&apikey=${process.env.API_KEY}`;
+      request.onload = function() {
+        if (this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(Error(request.statusText));
+        }
+      };
+      request.open("GET", url, true);
+      request.send();
+    });
+
+    promise.then(function(response) {
+      let body = JSON.parse(response);  //THIS line is a significant change from fetch and async
+      $('.showHumidity').text(body.Title); //This is different than the fetch and async- they used response.Title template literals
+      $('.showTemp').text(body.Director);  //- they used response.Title template literals
+    }, function(error) {
+      $('.showErrors').text(`There was an error processing your request: ${error.message}`);
+    });
   });
 });
